1ac76384d5d2da1c70bc7fd21d6bace5eeff79c8
Daniel Herrera <bundlehub@email.com>
2020-10-07 10:43:05 -0500

    saving work

--- stix_shifter_modules/db2/utils/transform.py
+++ stix_shifter_modules/db2/utils/transform.py
@@ -1,4 +1,8 @@
+import datetime
+
 # Transform from datetime.datetime(1,2,3) => miliseconds
 def time_transform(result):
-    result['STIME'] = result['STIME'].strftime("%s")
-    result['ETIME'] = result['ETIME'].strftime("%s")
+    for key in result:
+        if isinstance(result[key], datetime.datetime):
+            result['STIME'] = result['STIME'].strftime("%s")
+            result['ETIME'] = result['ETIME'].strftime("%s")
